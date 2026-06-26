8b0bf4eab688cb34334addbe873d21d7266dc6aa
abdelkarimnakkahi <nakkahi.abdelkarim@gmail.com>
2026-06-26 21:25:21 +0100

    add hero bottom

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,6 +3,11 @@ import dogImage1 from "../../assets/images/dog-image1.png";
 import dogImage2 from "../../assets/images/dog-image2.png";
 import dogImage3 from "../../assets/images/dog-image3.png";
 import discountImage from "../../assets/images/discount-badge.png";
+import dogFood from "../../assets/images/dog-food.png";
+import dogHumanHands from "../../assets/images/dog-human-hands.png";
+import authorAvatar from "../../assets/images/author-image.png";
+import signature from "../../assets/images/signature.svg";
+
 function Hero() {
   return (
     <section className="hero">
@@ -29,7 +34,30 @@ function Hero() {
           </div>
           <img src={dogImage2} alt="Dog Image" />
         </div>
-        <div className="bottom-hero"></div>
+        <div className="bottom-hero">
+          <div>
+            <img src={dogHumanHands} alt="Dog and human hands" />
+          </div>
+          <div className="author-content">
+            <h2>A pet is an animal kept mainly for companionship.</h2>
+            <div className="author-info">
+              <div className="author-detail">
+                <img
+                  className="author-avatar"
+                  src={authorAvatar}
+                  alt="Author avatar"
+                />
+                <div>
+                  <p className="author-name">Frank D. Hyden</p>
+                  <img src={signature} alt="Author signature" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <img src={dogFood} alt="Dog food" />
+          </div>
+        </div>
       </div>
     </section>
   );
